9acb2041af6b757d4b3ec1a6cefe48a00a8fcbf0
Jay Rathod <Jay@Jays-MacBook-Pro.local>
2020-10-09 17:54:25 +0400

    Small changes made

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -36,7 +36,7 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
       }}
       options={{
         legend: { display: false },
-        title: { display: true, text: `Current state in ${country}` },
+        title: { display: true, text: `COVID-19 Data for ${country}` },
       }}
     />
   ) : null;
